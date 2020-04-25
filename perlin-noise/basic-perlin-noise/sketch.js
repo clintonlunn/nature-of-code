709c492c15cfe8b4b48cfcd07aef5e2a39ba5018
Clinton Lunn <cpl09c@gmail.com>
2020-04-25 00:46:37 -0400

    basic perlin noise shown as an ellipse

--- perlin-noise/basic-perlin-noise/sketch.js
+++ perlin-noise/basic-perlin-noise/sketch.js
@@ -1,7 +1,15 @@
-function setup() { 
-  createCanvas(400, 400);
-} 
+let xoff = 0;
 
-function draw() { 
-  background(0);
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(50)
+  // var x = random(width)
+  var x = map(noise(xoff), 0, 1, 0, width)
+
+  xoff += .01;
+
+  ellipse(x, 200, 24, 24, width);
 }
