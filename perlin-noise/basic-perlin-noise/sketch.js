956a03b4177ce8b2c05836bd9f97621470b36cc2
Clinton Lunn <cpl09c@gmail.com>
2020-04-25 01:35:00 -0400

    scan across perlin noise space

--- perlin-noise/basic-perlin-noise/sketch.js
+++ perlin-noise/basic-perlin-noise/sketch.js
@@ -2,6 +2,8 @@
 
 // let xoff1 = 0;
 // let xoff2 = 10000;
+let inc = 0.01
+let start = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -9,17 +11,18 @@ function setup() {
 
 function draw() {
   background(50)
-  // var x = random(width)
   noFill();
   beginShape();
-
+  let xoff = start;
   for (let x = 0; x < width; x++) {
     stroke(200)
-    vertex(x, random(height))
+    let y = noise(xoff) * height
+    vertex(x, y)
+    xoff += inc;
   }
-
+  start += inc;
   endShape();
-  noLoop();
+  // noLoop();
 
   // var x = map(noise(xoff1), 0, 1, 0, width)
   // var y = map(noise(xoff2), 0, 1, 0, height)
